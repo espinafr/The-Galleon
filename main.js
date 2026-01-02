2f7736e72720876743f001b0a9153ddabf4d44d0
espinafr <theo.ruela@gmail.com>
2026-01-02 15:00:16 -0300

    Items details and changes to config retrieval. Now the config is updated every time the user access the index page or if it's older than 24 hours. Also added item details, a page where the user can see more information about a specific item based on its ID (the row it is in google sheets).

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const SPREADSHEET_ID = "1h1vPm6fOUxieO5uhXg3oGxNqT2ISKsu7_tmzfFNb8To";
 const CONFIG_GID = 1583366169;
-const SEASONALS_GID = 0;
-const RARE_ITEMS_GID = 1442647108;
-const SCROLLS_GID = 1428613482;
-const NPC_DROPS_GID = 349543872;
-const INGREDIENTS_GID = 573269167;
-const MODIFIED_ITEMS_GID = 1628198663;
-const MISCELLANEOUS_GID = 550507433;
+const ITEMS_GID = {
+  seasonals: 0,
+  rare_items: 1442647108,
+  scrolls: 1428613482,
+  npc_drops: 349543872,
+  ingredients: 573269167,
+  modified_items: 1628198663,
+  miscellaneous: 550507433
+}
 
 function sheetUrl(gid) {
   return `https://docs.google.com/spreadsheets/d/${SPREADSHEET_ID}/gviz/tq?gid=${gid}&tqx=out:json`;
@@ -49,8 +51,41 @@ function mapItemRow(row) {
     description: row.c[3]?.v ?? "",
     value: row.c[4]?.v ?? 0,
     demand: row.c[5]?.v ?? 0,
-    notes: row.c[6]?.v ?? ""
+    notes: row.c[6]?.v ?? "",
+    oldvalue: row.c[7]?.v ?? "N/A",
   };
+}
+
+async function retrieveConfig(specific_value=null) {
+  try {
+    let config = await loadKeyValueSheet(CONFIG_GID)
+    localStorage.setItem('last_config_update', Date.now());
+    for (const key in config) {
+      console.log(`Saving \"${key}\" in the local storage`)
+      localStorage.setItem(key, config[key]);
+    }
+    return specific_value ? config[specific_value] : true;
+  } catch (error) {
+    console.error("Error storing or retrieving config:", error);
+    return false
+  }
+}
+
+async function getConfig(value) {
+  try {
+    const storedConfig = localStorage.getItem(value);
+    if (storedConfig) {
+      if (Date.now() - localStorage.getItem('last_config_update') > 24 * 60 * 60 * 1000) {
+        console.log("Config is older than 24 hours, retrieving new config.");
+        return await retrieveConfig(value);
+      }
+      return storedConfig;
+    } else {
+      return await retrieveConfig(value);
+    }
+  } catch (error) {
+    console.error("Error reading from localStorage:", error);
+  }
 }
 
 function addDemand(demand) {
@@ -58,13 +93,16 @@ function addDemand(demand) {
     return `<span style="color: ${colors[demand-1]};">${"★".repeat(demand)}${"☆".repeat(5-demand)}</span>`;
 }
 
-function renderTable(items, death_crown_base_value) {
+function renderTable(items, item_type, death_crown_base_value) {
   const tbody = document.querySelector("tbody");
   tbody.innerHTML = "";
 
   for (const item of items) {
     const tr = document.createElement("tr");
-    tr.classList.add('border-b','border-slate-200','odd:bg-white','even:bg-orange-100');
+    tr.classList.add('border-b','border-slate-200','odd:bg-white','even:bg-orange-100', 'cursor-pointer', 'transition-colors','duration-200', 'hover:bg-orange-200');
+    tr.onclick = () => {
+      window.location.href = `../item_details.html?type=${item_type}&id=${encodeURIComponent(item.id)}`;
+    }
 
     tr.innerHTML = `
         <td class="p-3 align-top">
@@ -78,14 +116,8 @@ function renderTable(items, death_crown_base_value) {
         <td class="p-3 text-center text-slate-700">${parseFloat((item.value/death_crown_base_value).toFixed(2)).toLocaleString()}</td>
         <td class="p-3 text-center text-amber-500">${addDemand(item.demand)}</td>
         <td class="p-3 text-center text-slate-600">${item.notes}</td>
-    `;
+        `;
 
     tbody.appendChild(tr);
   }
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('.demandContainer').forEach(container => {
-		container.innerHTML = addDemand(container.getAttribute('data-demand', 1));
-	});
-});
